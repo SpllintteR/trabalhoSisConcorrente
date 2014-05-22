6d556ac349e175b891d1e2216dd3d63f30c16dfa
pmbweber <pmbweber@MC-3506.furb.br>
2014-05-21 21:32:20 -0300

    Inittial Convertion

--- TrabalhoSisConcorrente/src/gui/Main.java
+++ TrabalhoSisConcorrente/src/gui/Main.java
@@ -8,27 +8,13 @@ public class Main {
 
 	public static void main(final String[] args) {
 		Scanner scan = new Scanner(System.in);
-		int quantCidades = 0;
 		try {
-			System.out.print("Digite a quantidade de cidades a serem simuladas: ");
-			quantCidades = Integer.parseInt(scan.nextLine());
-			while (quantCidades > 100 || quantCidades <= 0) {
-				System.out.println("Quantidade de cidades não pode ser menor ou igual a 0 nem maior que 100");
-				System.out.print("Digite a quantidade de cidades a serem simuladas: ");
-				quantCidades = Integer.parseInt(scan.nextLine());
-			}			
-			
-			Cidade[] cidades = new Cidade[quantCidades];
-			for (int i = 0; i < quantCidades; i++) {
 				System.out.print("Digite a quantidade de meses a serem simulados: ");
 				int quantMeses = Integer.parseInt(scan.nextLine());
 				System.out.print("Digite o valor do intervalo (ms) entre cada mês: ");
 				long intervalo = Integer.parseInt(scan.nextLine());
-				cidades[i] = new Cidade(quantMeses, intervalo, i + 1);
-			}
-			for (Cidade cidade : cidades) {
-				cidade.start();
-			}
+				Cidade cidade = new Cidade(quantMeses, intervalo);
+				cidade.execute();
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		} finally {
